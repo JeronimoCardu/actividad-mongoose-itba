68cd4183a7f00440ee64f1921e2dd6aedb02f3c9
Jeronimo Cardu <jeronimocardu29@gmail.com>
2025-10-27 09:02:37 -0300

    creacion de rutas e intalacion de nodemon

---
+++ routes/projectRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const Project = require('../models/ProjectSchema');
+
+
+
+module.exports = router;
---
+++ routes/taskRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const Task = require('../models/TaskSchema');
+
+// Create a new task
+
+
+
+
+module.exports = router;
